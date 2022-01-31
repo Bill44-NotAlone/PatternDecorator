8aa43463e8e85ba7386bc4b661985847202873db
DemonEyes12 <anemnonov2@gmail.com>
2022-01-31 11:36:47 +0300

    Паттерн декоратор. Версия 0.1

---
+++ PatternDecorator/DecoratorLib/Beverage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoratorLib
+{
+    public abstract class Beverage
+    {
+        protected string description = "";
+        protected bool milk;
+        protected bool soy;
+        protected bool mocha;
+        protected bool whip;
+
+        public string Cost()
+        {
+            return milk + " " + soy +" " + mocha + " " + whip;
+        }
+
+        public string GetDescription()
+        {
+            return description;
+        }
+    }
+}
---
+++ PatternDecorator/DecoratorLib/DarkRoast.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoratorLib
+{
+    public class DarkRoast : Beverage
+    {
+        public DarkRoast()
+        {
+            description = "";
+        }
+    }
+}
